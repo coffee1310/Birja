3c0d6dbbb11fe7ec20637bed180846a47bba3610
Coffee <Coffee13102006@yandex.ru>
2024-07-09 22:45:22 +0300

    first page

--- BirjaSite/app/views.py
+++ BirjaSite/app/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView, FormView, ListView, DetailView
+
 # Create your views here.
-def index(request):
-    return HttpResponse("index.html")
+class indexPage(TemplateView):
+    template_name = "app/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+    def post(self, request, *args, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return self.render_to_response(context)
